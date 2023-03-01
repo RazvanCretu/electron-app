7d0baa8221f84bc736572c2763c3e8a60aee2a40
Razvy Cretu <razvan.cretu97@gmail.com>
2023-03-02 01:28:51 +0200

    adding some Routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Layout from "./components/Layout";
 import Search from "./screens/Search";
+import ProtectedLayout from "./components/ProtectedLayout";
+import Settings from "./screens/Settings";
+import LogIn from "./screens/LogIn";
+import Succes from "./screens/Succes";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="search" element={<Search />} />
+        <Route path="login" element={<LogIn />} />
+        <Route path="/succes" element={<Succes />} />
+        <Route element={<ProtectedLayout />}>
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Route>
     </Routes>
   );
