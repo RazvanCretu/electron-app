1825e1c27022be63e317fa18bd19fb358acf9b49
Razvy Cretu <razvan.cretu97@gmail.com>
2023-03-06 10:39:51 +0200

    Set up Apollo & Auth providers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import client from "./apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import { MemoryRouter as Router } from "react-router-dom";
 import { GlobalStyles } from "@mui/material";
 import App from "./App";
@@ -8,23 +10,30 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { Auth } from "./contexts/auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyles
-        styles={{
-          body: {
-            // WebkitAppRegion: "drag",
-            margin: 0,
-          },
-          "button, a": {
-            // WebkitAppRegion: "no-drag",
-          },
-        }}
-      />
-      <App />
+      <ApolloProvider client={client}>
+        <GlobalStyles
+          styles={{
+            body: {
+              // WebkitAppRegion: "drag",
+              // minHeight: "800px",
+              // minWidth: "1200px",
+              margin: 0,
+            },
+            "button, a": {
+              // WebkitAppRegion: "no-drag",
+            },
+          }}
+        />
+        <Auth>
+          <App />
+        </Auth>
+      </ApolloProvider>
     </Router>
   </React.StrictMode>
 );
