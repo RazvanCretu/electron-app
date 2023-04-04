b6fd6a14b0091e168b14ec606117b0144b966f14
Razvy Cretu <razvan.cretu97@gmail.com>
2023-04-04 23:31:57 +0300

    Load Electron version into UI

--- electron/preload.js
+++ electron/preload.js
@@ -1,6 +1,12 @@
 const { ipcRenderer, contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("electron", {
+  versions: {
+    // app: app.getVersion(),
+    electron: process.versions.electron,
+    chrome: process.versions.chrome,
+    node: process.versions.node,
+  },
   handleToken: (channel, cb) => {
     ipcRenderer.on("token", (event, ...args) => cb(...args));
   },
