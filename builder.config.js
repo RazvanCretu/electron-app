ba18fc1ef23ae494f46a398c3171bb193bde072f
Razvy Cretu <razvan.cretu97@gmail.com>
2023-04-11 03:14:41 +0300

    builder config

--- builder.config.js
+++ builder.config.js
@@ -1,10 +1,20 @@
+const pjson = require("./package.json");
+
 module.exports = {
   //  Electron Builder Settings
-  appId: "com.${name}",
+  appId: `com.electron.${pjson.name}`,
   artifactName: "${productName} setup.${ext}",
+  // The below is need so that `electron-builder` won't search for public/electron.js but rather in the paths included in `files`
+  // electron/electron.js our case.
   extends: null,
   files: ["build/**/*", "electron/**/*", "package.json"],
   directories: { output: "./dist/${version}" },
+  publish: {
+    provider: "github",
+    owner: "RazvanCretu",
+    repo: "electron-app",
+    token: "${env.GH_TOKEN}",
+  },
 };
 
 // appId: com.example.electron-cra
