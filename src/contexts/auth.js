9022fc782bffd8b89b87a3329bd5325554a6f918
Razvy Cretu <razvan.cretu97@gmail.com>
2023-04-17 18:39:51 +0300

    refactor and fix multiple run on electron token event

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,16 @@
 import { createContext, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { gql, useLazyQuery } from "@apollo/client";
+
+const QUERY_ME = gql`
+  query User {
+    me {
+      id
+      username
+      email
+    }
+  }
+`;
 
 const AuthContext = createContext();
 
@@ -14,57 +25,50 @@ const localToken = () => {
 export const useAuth = () => useContext(AuthContext);
 
 export const Auth = ({ children }) => {
+  const navigate = useNavigate();
   const [signIn, { data, loading, error, client, updateQuery }] = useLazyQuery(
-    gql`
-      query User {
-        me {
-          id
-          username
-          email
-        }
-      }
-    `,
+    QUERY_ME,
     { fetchPolicy: "network-only" }
   );
 
-  useEffect(() => {
-    const token = localToken();
-    if (token) {
-      signIn({
-        context: {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      });
-    }
-  }, [signIn]);
+  const isAuthenticated = data?.me && true;
 
-  window.electron.handleToken("token", (message) => {
-    const url = new URLSearchParams(message.replace("succes/", ""));
-    const params = Object.fromEntries(url.entries());
-
-    window.localStorage.setItem("token", params.token);
-
+  const logIn = (token) => {
     signIn({
       context: {
         headers: {
-          Authorization: `Bearer ${params.token}`,
+          Authorization: `Bearer ${token}`,
         },
       },
+    }).then((resp) => {
+      if (resp.data?.me) navigate("/dashboard");
     });
-  });
+  };
 
-  const isAuthenticated = data?.me && true;
-
-  const signOut = async () => {
+  const signOut = () => {
     updateQuery((prevData) => ({
+      ...prevData,
       me: null,
     }));
-    await client.clearStore();
+    client.clearStore();
 
     window.localStorage.removeItem("token");
   };
+
+  useEffect(() => {
+    const token = localToken();
+    if (token) logIn(token);
+
+    window.electron.handleToken("token", (message) => {
+      const url = new URLSearchParams(message.replace("succes/", ""));
+      const params = Object.fromEntries(url.entries());
+
+      window.localStorage.setItem("token", params.token);
+
+      logIn(params.token);
+    });
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <AuthContext.Provider
