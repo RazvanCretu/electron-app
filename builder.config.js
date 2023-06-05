b423c4c37f92d6fb98ae6fe6886312efca77dbb4
RazvanCretu <razvan.cretu97@gmail.com>
2023-06-05 21:27:37 +0300

    eslint

--- builder.config.js
+++ builder.config.js
@@ -3,17 +3,20 @@ const pjson = require("./package.json");
 module.exports = {
   //  Electron Builder Settings
   appId: `com.electron.${pjson.name}`,
+  // eslint-disable-next-line no-template-curly-in-string
   artifactName: "${productName} setup.${ext}",
   // The below is need so that `electron-builder` won't search
   // for public/electron.js but rather in the paths
   // included in `files` property. electron/electron.js our case.
   extends: null,
   files: ["build/**/*", "electron/**/*", "package.json"],
+  // eslint-disable-next-line no-template-curly-in-string
   directories: { output: "./dist/${version}" },
   publish: {
     provider: "github",
     owner: "RazvanCretu",
     repo: "electron-app",
+    // eslint-disable-next-line no-template-curly-in-string
     token: "${env.GH_TOKEN}",
   },
 };
