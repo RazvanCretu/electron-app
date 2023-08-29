b473b31a5feb970f24291e5d9d853adc5ad3107c
RazvanCretu <razvan.cretu97@gmail.com>
2023-08-29 22:36:58 +0300

    Custom Apollo Link for auth requests

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,33 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const localToken = () => {
+  // run client-side only
+  if (typeof window !== "undefined") {
+    const localToken = window.localStorage.getItem("token");
+    return localToken || undefined;
+  }
+};
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:1337/graphql",
+  // uri: "https://razvicodes.herokuapp.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localToken();
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "https://razvicodes.herokuapp.com/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
